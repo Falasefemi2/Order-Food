94910ad806b35c49193fa350f11dd7c40afe2bc8
Falasefemi2 <femifalase228@gmail.com>
2024-05-07 15:23:49 +0100

    change the welcome message

--- greet.go
+++ greet.go
@@ -5,15 +5,15 @@ import (
 )
 
 func greet(customerName string) {
-	fmt.Printf("%52s %s\n", "Namaste ", customerName)
-	fmt.Printf("%72s\n", "_/\\_ Welcome to Jaipur Bhojanalya! _/\\_ ")
+	fmt.Printf("%52s %s\n", "Welcome ", customerName)
+	fmt.Printf("%72s\n", "_/\\_ Welcome to Falase Femi! _/\\_ ")
 	fmt.Println()
 }
 
 func sayTata(customerName string) {
 	fmt.Println()
 	fmt.Printf("%17s", " ")
-	fmt.Printf("_/\\_ Thank you %v for visiting Jaipur Bhojanalya! _/\\_\n", customerName)
+	fmt.Printf("_/\\_ Thank you %v for visiting Falase Femi! _/\\_\n", customerName)
 	fmt.Printf("%55s\n", "We hope to see you again!")
 	fmt.Printf("%51s\n", "Have a nice day! ")
 }
